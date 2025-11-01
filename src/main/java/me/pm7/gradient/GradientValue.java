349a6c0a9dc25f4a8ce1704cdb3523e9e6271cc2
_Pm7 <155278755+Pm7-dev@users.noreply.github.com>
2025-10-31 22:01:26 -0500

    Delete src directory

--- src/main/java/me/pm7/gradient/GradientValue.java
+++
@@ -1,5 +0,0 @@
-package me.pm7.gradient;
-
-public record GradientValue<T>(T data, double position) {
-
-}
--- src/main/java/me/pm7/particle/ParticleType.java
+++
@@ -1,8 +0,0 @@
-package me.pm7.particle;
-
-public enum ParticleType {
-    CUBE,
-    CUBE_SHADELESS,
-    CIRCLE,
-    SQUARE
-}
